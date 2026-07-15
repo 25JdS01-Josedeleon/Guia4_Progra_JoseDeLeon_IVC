eabaff723e089f10158e6517abc98b4c4527de91
25JdS01-Josedeleon <jleondb25@gmail.com>
2026-07-15 09:07:27 -0600

    Add files via upload

---
+++ Lec35_Act1_Inc1.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Nombre: Jose de Leon\n" +
+            "Grado: IVC\n"+
+            "Clave: 10\n"+
+            "==Conversiones==");
+        int pago;
+        string producto;
+        Console.WriteLine("Digite el nombre del producto");
+        producto = Console.ReadLine();
+        Console.WriteLine("Dame el precio del producto; ");
+        int precio= Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("digite el valor del bilete: ");
+        pago = Convert.ToInt32(Console.ReadLine());
+        int cambio = pago - precio;
+        Console.WriteLine("Su cambio es: " + cambio);
+    }
+}
